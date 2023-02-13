3d7160e12bc56c4d3d097ca086b7c4dc612ad948
Dav1Samu3l <davidev.front@gmail.com>
2023-02-13 19:30:25 -0300

    entrega de form. simples

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var mensagemEnviada = document.getElementById("mensagem-enviada");
+    if (window.confirm("Tem certeza de que deseja enviar os dados?")) {
+      mensagemEnviada.innerHTML = "Dados enviados com sucesso!";
+      mensagemEnviada.style.display = "block";
+    }
+  });
+  
