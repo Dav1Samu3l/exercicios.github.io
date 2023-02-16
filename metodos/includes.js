1f81904b1a08295f5a5ecc9a5928d4204fc2c65c
Dav1Samu3l <davidev.front@gmail.com>
2023-02-16 20:27:11 -0300

    metodos

---
+++ metodos/includes.js
@@ -0,0 +1,9 @@
+
+
+let fruits = ["apple", "banana", "orange"];
+
+if (fruits.includes("apple")) {
+    console.log("There is an apple in the list of fruits.");
+} else {
+    console.log("There is no apple in the list of fruits.");
+}
